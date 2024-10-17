f9e091f5d56a8e16ac8772a8a5fec9b166fc3249
favour-rite <figwe000@gmail.com>
2024-10-17 21:45:32 +0100

    update

---
+++ classWork/Radius.py
@@ -0,0 +1,3 @@
+print(input(Enter a number"))
+pie= 3.142
+Result =number *number* 3.142
---
+++ classWork/number.py
@@ -0,0 +1,11 @@
+number= int (input (" enter a number between 0 and 999: "))
+last number = number %100
+first number = number // 10
+second number = number/ / 10
+middle nummber = second number % 10
+
+
+
+
+
+

c30b24cb387b7496018e53117de651f827c264ad
favour-rite <figwe000@gmail.com>
2024-10-25 08:33:49 +0100

    Python Assignment

---
+++ classWork/Count.py
@@ -0,0 +1,11 @@
+number1 = int (input("Enter a value: "))
+number2 = int (input("Enter a value: "))
+number3 = int (input ("Enter a value: "))
+number = 0
+
+if number1 <= number2:
+	
+else number % 3== 0:
+count +=1
+	number += 1
+print (count)

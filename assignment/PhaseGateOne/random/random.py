cf5d1d96decab9c0309c77fe3736ad9bd66d2bbe
favour-rite <figwe000@gmail.com>
2024-12-02 17:00:31 +0100

    update

---
+++ assignment/PhaseGateOne/random/random.py
@@ -0,0 +1,31 @@
+
+import random
+score = 0
+fail = 0
+passed = 0
+
+for count in range(1,10): 
+	random_digit = random.randrange(1,60)
+	random_digit2 = random.randrange(0,100)
+	result = random_digit - random_digit2
+
+
+user_input = (input(" enter your answer "))
+
+if user_input > 10:
+	pass +=
+
+elif user_input < 10:	
+	fail += 
+
+print ("Your total score is" + fail, passed )
+
+
+
+
+
+
+
+
+
+
---
+++ assignment/PhaseGateOne/sum/sum_random_number.py
@@ -0,0 +1,14 @@
+
+import random
+count = 0
+	random_digit = random.randint(1,60)
+	random_digit = random.randint(45,100)
+	result 
+
+user_Input = (input(" sum the numbers "))
+
+
+if user_Input :
+	print("true")
+else:
+	print("false") 
